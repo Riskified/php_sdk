0f64287427463b61aa703c8c4dc2b7f0d95d2ccd
Eyal <eyal@riskified.com>
2014-03-11 13:19:43 +0200

    Creating the skeleton classes.

---
+++ lib/order_info.php
@@ -0,0 +1,26 @@
+<?php
+
+class OrderInfo {
+
+  /**
+   * The OrderInfo class should contain the following fields:
+   * id
+   * name
+   * email
+   * total_spent
+   * cancel_reason
+   * created_at
+   * closed_at
+   * currency
+   * updated_at
+   * gateway
+   * browser_ip
+   * cart_token
+   * note
+   * referring_site
+   * total_price
+   * total_discounts
+   * discount_codes
+   */
+}
+
---
+++ lib/payment_details.php
@@ -0,0 +1,14 @@
+<?php
+
+class PaymentDetails {
+
+  /**
+   * The PaymentDetails class should contain the following fields:
+   * credit_card_bin
+   * avs_result_code
+   * cvv_result_code
+   * credit_card_number
+   * credit_card_company
+   */
+}
+
