6a0e625c56bef64f289f8c472669f9291ccc4e29
Eyal <eyal@riskified.com>
2014-03-11 13:04:56 +0200

    Testing whether the creation of the empty classes looks goods.

---
+++ lib/customer.php
@@ -0,0 +1,15 @@
+<?php
+
+class Customer {
+
+  /**
+   * The Customer class should contain the following fields:
+   * created_at
+   * email
+   * first_name
+   * last_name
+   * id
+   * note
+   * orders_count
+   */ 
+}
