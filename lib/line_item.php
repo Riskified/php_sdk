09c8264863c2d2f18d09b4105d31052b99c3c036
Eyal <eyal@riskified.com>
2014-03-11 13:08:53 +0200

    Testing whether the creation of the empty classes looks goods.

---
+++ lib/line_item.php
@@ -0,0 +1,15 @@
+<?php
+
+class LineItem {
+
+  /** 
+   * The LineItem class should contain the following fields:
+   * price
+   * product_id
+   * quantity
+   * sku
+   * title
+   * public_url
+   */ 
+
+}
