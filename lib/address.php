2583920e39a109a07bd9b35bc1d5053cf7eb48b9
Eyal <eyal@riskified.com>
2014-03-11 12:58:14 +0200

    Testing whether the creation of the empty classes looks goods.

---
+++ lib/address.php
@@ -0,0 +1,22 @@
+<?php
+
+class Address {
+
+  /**
+   * The address class should contain the following fields:
+   * - first_name
+   * - last_name
+   * - name
+   * - company
+   * - address_1 
+   * - address_2
+   * - city
+   * - country_code (2 letter country code)
+   * - country
+   * - province
+   * - province_code
+   * - zip
+   * - phone
+   */
+}
+
