9bb459a71af6a206c58e967f8c45e4d70fc1a948
Assaf Feldman <assaf@riskified.com>
2014-03-24 00:41:36 +0200

    added Copyright header

--- src/Riskified/Common/Riskified.php
+++ src/Riskified/Common/Riskified.php
@@ -1,6 +1,21 @@
 <?php
 error_reporting(E_STRICT);
 
+/**
+ * Copyright 2013-2014 Riskified.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 function __autoload($class) {
 	if (strpos($class, 'Riskified\\') == 0) {
         $parts = explode('\\', $class);
--- src/Riskified/OrderWebhook/Model/PaymentDetails.php
+++ src/Riskified/OrderWebhook/Model/PaymentDetails.php
@@ -1,4 +1,20 @@
-<?php namespace Riskified\OrderWebhook\Model;
+<?php
+/**
+ * Copyright 2013-2014 Riskified.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+namespace Riskified\OrderWebhook\Model;
+
 class PaymentDetails extends AbstractModel {
 
     protected $_fields = [
