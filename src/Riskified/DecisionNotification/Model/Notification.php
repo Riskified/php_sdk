9b1d93e7413b07a98ccdbc131985fdc544bfe4c6
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2019-07-17 16:16:34 -0400

    add spacing to Notification to mirror master

--- src/Riskified/DecisionNotification/Model/Notification.php
+++ src/Riskified/DecisionNotification/Model/Notification.php
@@ -13,17 +13,21 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 /**
  * Shop URL is available as a notification parameter depending on your account's setup; please contact your Integration Engineer or Account Manager if you have questions on this.
  * It is a NON-best-practice to use shop URL in the notifications programmatically as this field will not be supported long term in API notifications.
  */
+
 use Riskified\DecisionNotification\Exception;
+
 /**
  * Class Notification
  * Parses and validates Decision Notification callbacks from Riskified
  * @package Riskified\DecisionNotification\Model
  */
 class Notification {
+
     /**
      * @var string Order ID
      */
@@ -48,9 +52,11 @@ class Notification {
      * @var string Decision Code of Decision
      */
     public $decisionCode;
+
     protected $signature;
     protected $headers;
     protected $body;
+
     /**
      * Inits and validates the request.
      * @param $signature Signature An instance of a Signature class that handles authentication
@@ -62,9 +68,11 @@ class Notification {
         $this->signature = $signature;
         $this->headers = $headers;
         $this->body = $body;
+
         $this->test_authorization();
         $this->parse_body();
     }
+
     /**
      * assets that the request authentication is valid
      * @throws \Riskified\DecisionNotification\Exception\AuthorizationException on HMAC mismatch
@@ -76,6 +84,7 @@ class Notification {
         if ($remote_hmac != $local_hmac)
             throw new Exception\AuthorizationException($this->headers, $this->body, $local_hmac, $remote_hmac);
     }
+
     /**
      * extracts parameters from HTTP POST body
      * @throws \Riskified\DecisionNotification\Exception\BadPostJsonException on bad or missing parameters
@@ -85,17 +94,21 @@ class Notification {
         if (!array_key_exists('order', $body))
             throw new Exception\BadPostJsonException($this->headers, $this->body);
         $order = $body->{'order'};
+
         if (!array_key_exists('id', $order) || !array_key_exists('status', $order))
             throw new Exception\BadPostJsonException($this->headers, $this->body);
+
         //foreach($order as $key => $value)
         //    $this->$key = $value;
         $this->id = $order->{'id'};
         $this->status = $order->{'status'};
         $this->oldStatus = $order->{'old_status'};
         $this->description = $order->{'description'};
+
         if (array_key_exists('category', $order)) {
             $this->category = $order->{'category'};
         }
+        
         if (array_key_exists('decision_code', $order)) {
             $this->decisionCode = $order->{'decision_code'};
         }
