e9fac91f8256fead564f101cc3edf43ba74e8c45
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2019-08-21 12:59:42 -0400

    make liability shift optional parameter

--- src/Riskified/OrderWebhook/Model/AuthenticationResult.php
+++ src/Riskified/OrderWebhook/Model/AuthenticationResult.php
@@ -28,7 +28,7 @@ class AuthenticationResult extends AbstractModel
         'cavv' => 'string optional',
         'trans_status' => 'string /^(:?Y|N|U|A|C|D|R|I)$/ optional',
         'trans_status_reason' => 'string /^(:?01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|80|99)$/ optional',
-        'liability_shift' => 'boolean'
+        'liability_shift' => 'boolean optional'
     );
 
 }
