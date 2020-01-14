7131e1dfad837e6fa1e2b41f361cec0ae1379ef1
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2020-01-14 14:26:51 -0500

    add device_id to session_details

--- src/Riskified/OrderWebhook/Model/SessionDetails.php
+++ src/Riskified/OrderWebhook/Model/SessionDetails.php
@@ -26,6 +26,7 @@ class SessionDetails extends AbstractModel {
         'cart_token' => 'string',
         'browser_ip' => 'string /^(:?[0-9a-f]{0,5}[:\.])+[0-9a-f]{0,4}$/i',
         'source' => 'string /^(:?desktop_web|mobile_app|web|mobile_web|other)$/',
+        'device_id' => 'string optional',
         'referring_site' => 'string optional'
     );
 }
