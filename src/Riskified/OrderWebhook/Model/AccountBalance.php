6d23dd5c499e90dd00f5cd28bde895cabe0dbd4c
Diogo Yaguas <diogo.yaguas@riskified.com>
2026-05-28 14:54:43 +0100

    feat: add AccountIdentity and AccountBalance models

---
+++ src/Riskified/OrderWebhook/Model/AccountBalance.php
@@ -0,0 +1,30 @@
+<?php namespace Riskified\OrderWebhook\Model;
+/**
+ * Copyright 2013-2015 Riskified.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * Class AccountBalance
+ * Represents account balance information from a financial data provider.
+ * @package Riskified\OrderWebhook\Model
+ */
+class AccountBalance extends AbstractModel {
+
+    protected $_fields = array(
+        'available_balance' => 'float',
+        'service_name' => 'string /^(:?plaid|mx|stripe|truelayer|klarna|visa|mastercard|yodlee)$/',
+        'updated_at' => 'date',
+        'currency_code' => 'string /^[A-Z]{3}$/i'
+    );
+}
