2c6e93fa8f2e5cbe68f6475f63cce2fc08fe14cf
Tomer Parizer <tomer.parizer@riskified.com>
2026-06-09 15:39:03 +0100

    DEV-132452: add ai_agent field and source value to Order

--- src/Riskified/OrderWebhook/Model/Order.php
+++ src/Riskified/OrderWebhook/Model/Order.php
@@ -55,7 +55,7 @@ class Order extends AbstractModel {
         'landing_site' => 'string optional',
         'landing_site_ref' => 'string optional',
         'location_id' => 'string optional',
-        'source' => 'string /^(:?desktop_web|mobile_app|mobile_web|web|chat|third_party|phone|in_store|shopify_draft_order|unknown|mobile_app_android|mobile_app_ios)$/',
+        'source' => 'string /^(:?desktop_web|mobile_app|mobile_web|web|chat|third_party|phone|in_store|shopify_draft_order|unknown|mobile_app_android|mobile_app_ios|ai_agent)$/',
         'source_identifier' => 'string optional',
         'source_name' => 'string optional',
         'source_url' => 'string optional',
@@ -76,6 +76,7 @@ class Order extends AbstractModel {
         'order_type' => 'string optional',
         'partner_sub_merchant_id' => 'string optional',
         'submission_reason' => 'string optional',
+        'ai_agent' => 'string /^(?:chatgpt|gemini|copilot|perplexity)$/ optional',
 
         'shipping_address' => 'object \Address optional',
         'billing_address' => 'object \Address optional',
